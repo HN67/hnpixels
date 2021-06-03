0655d902f54c66289e15a298293f3714eb08f69c
HN67 <hn67allardkids@gmail.com>
2021-06-03 12:21:50 -0600

    Create Endpoint class

--- hnpixels/core.py
+++ hnpixels/core.py
@@ -113,11 +113,160 @@ class Ratelimiter:
         """Blocks until internal guard time is passed, at which point it is safe to operate."""
         current = time.time()
         if current < self.guard_time:
+            logger.info("Sleeping for %.2f seconds", self.guard_time - current)
             time.sleep(self.guard_time - current)
+
+
+class RatelimitError(requests.HTTPError):
+    """Specific HTTPError that indicates the error was due to ratelimiting.
+
+    Therefore it will be most likely safe to retry later.
+    """
+
+
+TMR: int = requests.codes.too_many_requests  # pylint: disable=no-member
+
+
+@dataclasses.dataclass(init=False)
+class Endpoint:
+    """Manages an endpoint.
+
+    Tracks whether the endpoint can be communicated with,
+    rate limits, etc.
+    """
+
+    method: str
+    url: str
+    headers: t.Optional[t.Mapping[str, str]] = None
+
+    def __init__(
+        self, method: str, url: str, *, headers: t.Optional[t.Mapping[str, str]] = None
+    ) -> None:
+        """Constructs an Endpoint.
+
+        Requests will be made to the given URL,
+        and the headers given will be used as default headers,
+        with keys overridden by headers provided to the call.
+        """
+        self.url = url
+        self.method = method
+        self.headers = headers
+
+        self.ratelimiter = Ratelimiter()
+        # denotes whether the endpoint has actually been successfully queried,
+        # for ratelimit headers, etc
+        self._active = False
+
+    def update_ratelimiter(self, headers: t.Mapping[str, str]) -> None:
+        """Updates (unlock) the given ratelimiter using the following headers:
+
+        remaining: requests-remaining
+        limit: requests-limit
+        reset: requests-reset
+        """
+        logger.debug(headers)
+        try:
+            self.ratelimiter.unlock(
+                remaining=int(headers["requests-remaining"]),
+                limit=int(headers["requests-limit"]),
+                reset=float(headers["requests-reset"]),
+            )
+        except KeyError:
+            try:
+                self.ratelimiter.unlock(
+                    remaining=0, limit=0, reset=float(headers["cooldown-reset"])
+                )
+            except KeyError:
+                try:
+                    self.ratelimiter.unlock(
+                        remaining=0, limit=0, reset=float(headers["retry-after"])
+                    )
+                except KeyError as error:
+                    raise KeyError(
+                        f"No ratelimit headers found in {headers}"
+                    ) from error
+
+        # todo handle 'retry-after' from potential anti-spam
+        # also consider requests-period the new header
+        # also handle ratelimits for arbitrary endpoint (dict?)
+        # rather than a static number of limiters
+
+    def activate(self) -> None:
+        """Performs a HEAD request to load ratelimit information"""
+        response = requests.head(self.url, headers=self.headers)
+        if response.ok:
+            self.update_ratelimiter(response.headers)
+        # 429 isnt a fatal error here
+        # To Many Requests (i.e. ratelimited) code
+        elif response.status_code == TMR:
+            self.update_ratelimiter(response.headers)
+            raise RatelimitError("Received 429", response=response)
+        else:
+            response.raise_for_status()
+
+    def fetch(self, **kwargs: t.Any) -> requests.Response:
+        """Fetches the endpoint, passing along kwargs to requests.request.
+
+        Uses default headers unionized with provided headers if provided.
+        """
+
+        if not self._active:
+            try:
+                self.activate()
+            except RatelimitError:
+                # Try again, should succeed
+                try:
+                    self.activate()
+                except requests.HTTPError as error:
+                    raise requests.HTTPError(
+                        f"Failed to access endpoint {self}"
+                    ) from error
+            except requests.HTTPError as error:
+                # reraise
+                raise requests.HTTPError(f"Failed to access endpoint {self}") from error
+            # One of the above activates should have succeded
+            # to reach this point
+            self._active = True
+
+        # Start with an empty dict and attempt to update it with default and provided headers
+        headers: t.MutableMapping[str, str] = {}
+        if self.headers:
+            headers.update(self.headers)
+        try:
+            headers.update(kwargs["headers"])
+        except KeyError:
+            pass
+
+        # Attempt to ensure ratelimit compliance
+        self.ratelimiter.lock()
+        response = requests.request(
+            # If headers is empty we dont both passing it at all
+            self.method,
+            self.url,
+            headers=headers if headers else None,
+            **kwargs,
+        )
+
+        if response.ok:
+            # Maintain ratelimiter
+            self.update_ratelimiter(response.headers)
+            # Succesful response can be returned
+            return response
+        # Update ratelimiter, allows calling code to catch and recall
+        elif response.status_code == TMR:
+            self.update_ratelimiter(response.headers)
+            raise RatelimitError("Received 429", response=response)
+        # might be dumb but raising custom httperror since raise_for_status
+        # isnt guaranteed too from a typecheck perspective
+        else:
+            raise requests.HTTPError("Bad response from endpoint", response=response)
 
 
 # TODO use HEAD on startup instead of warmup to get ratelimit position
 # TODO logging for all behaviour/requets not just setting
+# TODO maybe dont hard crash on http error codes?
+# depends on code/method but a failed request may not neccesitate halting
+# especially since we sometimes just get random server errors
 class Painter:
     """Client object for interacting with the Pixels API.
 
@@ -177,12 +326,26 @@ class Painter:
                 reset=float(headers["requests-reset"]),
             )
         except KeyError:
-            limiter.unlock(remaining=0, limit=0, reset=int(headers["cooldown-reset"]))
+            try:
+                limiter.unlock(
+                    remaining=0, limit=0, reset=float(headers["cooldown-reset"])
+                )
+            except KeyError:
+                try:
+                    limiter.unlock(
+                        remaining=0, limit=0, reset=float(headers["retry-after"])
+                    )
+                except KeyError as error:
+                    raise KeyError(
+                        f"No ratelimit headers found in {headers}"
+                    ) from error
+
         # todo handle 'retry-after' from potential anti-spam
         # also consider requests-period the new header
         # also handle ratelimits for arbitrary endpoint (dict?)
         # rather than a static number of limiters
 
+    # TODO Handle 410 Gone Error specifically, and 429 Ratelimit
     def colour(self, x: int, y: int) -> Colour:
         """Returns the colour at the specified position."""
         self._get_pixel_limiter.lock()
@@ -197,14 +360,20 @@ class Painter:
         return Colour.from_hex(response.json()["rgb"])
         # return response.json()["rgb"]
 
-    def paint(self, x: int, y: int, colour: Colour) -> None:
+    # TODO Handle 429 Ratelimit without error
+    def paint(self, x: int, y: int, colour: Colour, *, check: bool = True) -> None:
         """Sets the colour of a pixel at the specified position.
+
+        If `check` is true, the method will call .colour before and after
+        waiting to short-circuit as soon as possible if the pixel is already
+        the correct colour, and avoid wasting set pixel requests.
 
         May block for a significant period to obey ratelimits.
         """
         # We want to avoid needlessly setting a pixel as much as possible
         # set_pixel ratelimit is extremely low, while other endpoints like get_pixel
         # have much higher limits.
+        # TODO wrap in try blocks, also add a parameter to check or not
         if self.colour(x, y) == colour:
             logger.info(
                 "pixel at x=%s,y=%s is already the correct color %s", x, y, colour.hex()
@@ -220,6 +389,7 @@ class Painter:
                 "pixel at x=%s,y=%s is already the correct color %s", x, y, colour.hex()
             )
             return
+        # TODO handle out-of-canvas pixels? maybe? need to check the response to such a request
         response = requests.post(
             self.endpoint("/set_pixel"),
             headers=self.headers,
@@ -239,6 +409,7 @@ class Painter:
         response.raise_for_status()
         return (response.json()["width"], response.json()["height"])
 
+    # TODO Handle 410 Gone Error specifically, and 429 Ratelimit
     def sketch(self) -> Sketch:
         """Returns the current state of the canvas."""
         self._get_canvas_limiter.lock()
